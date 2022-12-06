f3903bebbf7af6c2d61feedfbb52b200d28083d2
Hiro Protagonist <shd327@gmail.com>
2022-12-06 15:48:30 -0700

    added category update code

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -32,16 +32,44 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    const category = await Category.create(req.body)
+
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(400).json(err);
+  }
   // create a new category
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+  try {
+    const category = await Category.update(
+      {
+        category_name: req.body.category_name
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+
+    )
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(400).json(err);
+  }
   // update a category by its `id` value
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+
+  } catch {
+
+  }
 });
 
 module.exports = router;
