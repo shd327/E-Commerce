0818a2e660fca315bdcfd3f29ea9d33ad9d15e80
Hiro Protagonist <shd327@gmail.com>
2022-12-06 14:46:51 -0700

    added product model code

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -2,11 +2,45 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+
+      references: {
+        model: 'Category',
+        key: 'id',
+        unique: false
+      }
+    }
   },
   {
     sequelize,
