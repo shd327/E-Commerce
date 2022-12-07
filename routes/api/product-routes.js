2ada090a455d28c6fa8d4ed7b5608973e159e4ec
Hiro Protagonist <shd327@gmail.com>
2022-12-07 15:33:40 -0700

    added routes for product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -17,13 +17,32 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      where: {
+        id: req.params.id
+      }
+
+    })
+    res.status(200).json(productData)
+
+  } catch (err) {
+    res.status(400).json(err)
+  }
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    const productData = await Product.create(req.body)
+    res.status(200).json(productData)
+  }
+  catch (err) {
+    res.status(400).json(err)
+  }
   /* req.body should look like this...
     {
       product_name: "Basketball",
