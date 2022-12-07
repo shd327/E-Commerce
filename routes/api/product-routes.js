622c8c8a337fc463eaa8536355b12a2a0db05932
Hiro Protagonist <shd327@gmail.com>
2022-12-07 15:45:14 -0700

    finished up tag/product routeS

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -115,8 +115,19 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
+
 
 module.exports = router;
