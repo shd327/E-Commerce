2a35d0dd9f71b2c4ebb1be5212a60224d3257078
Hiro Protagonist <shd327@gmail.com>
2022-12-26 10:46:26 -0700

    fix product create route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -39,13 +39,13 @@ router.get('/:id', async (req, res) => {
 
 // create new product
 router.post('/', async (req, res) => {
-  try {
-    const productData = await Product.create(req.body)
-    res.status(200).json(productData)
-  }
-  catch (err) {
-    res.status(400).json(err)
-  }
+  // try {
+  //   const productData = await Product.create(req.body)
+  //   res.status(200).json(productData)
+  // }
+  // catch (err) {
+  //   res.status(400).json(err)
+  // }
 
   Product.create(req.body)
     .then((product) => {
@@ -60,13 +60,18 @@ router.post('/', async (req, res) => {
         return ProductTag.bulkCreate(productTagIdArr);
       }
       // if no product tags, just respond
-      res.status(200).json(product);
+      // res.status(200).json(product);
+
     })
+
     .then((productTagIds) => res.status(200).json(productTagIds))
+
     .catch((err) => {
       console.log(err);
       res.status(400).json(err);
     });
+
+
 });
 
 // update product
